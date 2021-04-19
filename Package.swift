ce40cdc20d5cb79bb8fa514885d752de485496e6
Xiao Xiao <xx2004xiamen@gmail.com>
2021-04-20 01:11:12 +0900

    Use swift-crypto instead of commoncrypto

--- Package.swift
+++ Package.swift
@@ -1,13 +1,19 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "OhhAuth",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .library(name: "OhhAuth", targets: ["OhhAuth"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.6")
+    ],
     targets: [
-        .target(name: "OhhAuth", path: ".")
+        .target(name: "OhhAuth", dependencies: [.product(name: "Crypto", package: "swift-crypto")], path: ".")
     ]
 )
